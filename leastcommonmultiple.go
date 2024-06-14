ca61e7c760b4a5e5367718e3f15af8955a05dd67
JNGR1997 <jngrobertson@gmail.com>
2024-06-14 10:35:48 +0200

    Update

---
+++ leastcommonmultiple.go
@@ -0,0 +1,18 @@
+package main 
+
+import "fmt"
+
+func main() {
+  fmt.Println(lcm(18,14))
+}
+
+func gcd(a, b int) int {
+  for b != 0 {
+    a, b = b, a%b
+  }
+  return a
+}
+
+func lcm(a, b int) int {
+  return a*(b/gcd(a,b))
+}
