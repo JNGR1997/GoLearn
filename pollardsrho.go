cc5c8275e715c6e524f5146431449fbf3a839f32
JNGR1997 <jngrobertson@gmail.com>
2024-06-15 07:16:31 +0200

    Update pollardsrho.go

--- pollardsrho.go
+++ pollardsrho.go
@@ -13,14 +13,18 @@ func gcd(a, b int) int {
   return a
 }
 
+func rhoPolynomial(x, n int) int {
+  return (x*x + 1)%n
+}
+
+
 func rho(n int) int {
   x := 2
   y := 2
   d := 1
   for d == 1 {
-    x = (x*x + 1) %n
-    y = (y*y*y*y + 2*y*y + 2) %n
-    fmt.Println(x,y,d)
+    x = rhoPolynomial(x,n)
+    y = rhoPolynomial(rhoPolynomial(y,n),n)
     if x-y > 0 {
       d = gcd(n, x-y)
     } else {
