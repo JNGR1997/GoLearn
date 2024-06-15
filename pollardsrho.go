ab4975d7cee75d5cfcd08b0f8a943b3cdde80336
JNGR1997 <jngrobertson@gmail.com>
2024-06-15 05:59:16 +0200

    Update

---
+++ pollardsrho.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println(rho(6))
+}
+
+func gcd(a, b int) int {
+  for b!= 0 {
+    a, b = b, a%b
+  }
+  return b
+}
+
+func rho(n int) int {
+  x := 2
+  y := 2
+  d := 1
+  for d == 1 {
+    x = (x*x + 1) %n
+    y = (y*y*y*y + 2*y*y + 2) %n
+    if x-y > 0 {
+      d = gcd(n, x-y)
+    } else {
+      d = gcd(n, y-x)
+    }
+  }
+  return d
+}
