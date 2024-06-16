4bdaafc942ec8a52b59edad4c4428e7acfffc576
JNGR1997 <jngrobertson@gmail.com>
2024-06-16 14:14:52 +0200

    Update

---
+++ smooth.go
@@ -0,0 +1,20 @@
+package main 
+
+import "fmt"
+
+func main() {
+  fmt.Println(isSmooth(33))
+}
+
+func isSmooth(n int) bool {
+  primes := [6]int{2,3,5,7,11,13}
+  for _, s := range primes {
+    for n%s == 0 {
+      n = n/s
+    }
+  }
+  if n == 1 {
+    return true
+  }
+  return false
+}
