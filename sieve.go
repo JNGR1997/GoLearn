7568048e5279925275b6b5aae9ff9bbb7a0186fd
JNGR1997 <jngrobertson@gmail.com>
2024-06-18 07:14:21 +0200

    Update

---
+++ sieve.go
@@ -0,0 +1,15 @@
+package main 
+
+import "fmt"
+
+func main() {
+}
+
+func sieve(a, b int) {
+  for i := a, i<b, i++ {
+    c, d = boolSmooth(i)
+    if c == 1 {
+      fmt.Println(d)
+    }
+  }
+}
