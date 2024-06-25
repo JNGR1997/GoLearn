7e20ef624dfe1c27e1338a054007394750cafe8a
JNGR1997 <jngrobertson@gmail.com>
2024-06-25 06:36:57 +0200

    Update modularinverse.go

--- modularinverse.go
+++ modularinverse.go
@@ -5,6 +5,11 @@ import "fmt"
 func main() {
 }
 
-func modInverse(a,n int) int {
-  
+func modInverse(a,n int) (bool, int) {
+  for b := 1, b < n, b++ {
+    if a*b % n == 1 {
+      return true, b
+    }
+  }
+  return false, 0
 }
