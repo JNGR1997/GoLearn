18908f1369be5c6cd60db0ca57ea1ae964026132
JNGR1997 <jngrobertson@gmail.com>
2024-06-26 06:51:16 +0200

    Update modularinverse.go

--- modularinverse.go
+++ modularinverse.go
@@ -13,3 +13,23 @@ func modInverse(a,n int) (bool, int) {
   }
   return false, 0
 }
+
+func euclideanInverse(a, m int) {
+  n := m
+  b := a
+  q := (n - n%b)/b
+  r := n%b
+  t1 := 0
+  t2 := 1
+  t3 := t1 - q*t2
+  for r != 0 {
+    n = b
+    b = r
+    q = (n - n%b)/b
+    r = n%b
+    t1 = t2
+    t2 = t3
+    t3 = t1 - q*t2
+    fmt.Println(n,b,q,r,t1,t2,t3)
+  }
+}
