a57a690900e100a553a47e7d5712a2762882aebc
JNGR1997 <jngrobertson@gmail.com>
2024-07-09 07:17:36 +0200

    Update sudoko.go

--- sudoko.go
+++ sudoko.go
@@ -8,7 +8,7 @@ func checkCells(a,b,c,d,e,f,g,h,i [9]bool) ([9]bool, [9]bool, [9]bool, [9]bool, 
 
 func numOptions(a [9]bool) int {
   b := 0
-  for c,d := range a {
+  for _,d := range a {
     if d==true {
       b++
     }
