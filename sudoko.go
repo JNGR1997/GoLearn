9094b8baef17827961e63fe226d4e56727d7f805
JNGR1997 <jngrobertson@gmail.com>
2024-07-30 07:20:06 +0200

    Update sudoko.go

--- sudoko.go
+++ sudoko.go
@@ -68,6 +68,13 @@ func twoOptions(a [9][9]bool) [9][9]bool {
     for d,e := range a {
       if b != d {
         if numOptions(sudUnion(c,e))==2 {
+          for f,g := range a {
+            if f != b {
+              if f != d {
+              
+              }
+            }
+          }
         }
       }
     }
