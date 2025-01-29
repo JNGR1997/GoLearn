599cb5c07700de001d239c88a8e2e6d2f32667eb
JNGR1997 <jngrobertson@gmail.com>
2025-01-29 13:09:50 +0200

    Update sieve.go

--- sieve.go
+++ sieve.go
@@ -6,22 +6,16 @@ func main() {
 	sieve(18, 323)
 }
 
-func boolSmooth(n int) (int, [6]int, [6]int) {
+func boolSmooth(n int) (int, [6]int) {
 	primes := [6]int{2, 3, 5, 7, 11, 13}
 	var a [6]int
-	var c [6]int
 	for b, s := range primes {
 		for n%s == 0 {
 			n = n / s
-			c[b] = c[b] + 1
-			if a[b] == 0 {
-				a[b] = 1
-			} else {
-				a[b] = 0
-			}
+			a[b] = (a[b]+1)%2
 		}
 	}
-	return n, a, c
+	return n, a
 }
 
 func sieve(a, b int) {
@@ -29,9 +23,9 @@ func sieve(a, b int) {
 	i := a
 	var f [7][6]int
 	for e < 7 {
-		c, d, g := boolSmooth(i * i % b)
+		c, d := boolSmooth(i * i % b)
 		if c == 1 {
-			fmt.Println(i, g)
+			fmt.Println(i, d)
 			f[e] = d
 			e++
 		}
