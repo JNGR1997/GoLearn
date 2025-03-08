91cd395050aebf14e6d48651c469cf1fbc743d3d
JNGR1997 <jngrobertson@gmail.com>
2025-03-08 21:26:39 +0200

    Update lenstraellipticcurve.go

--- lenstraellipticcurve.go
+++ lenstraellipticcurve.go
@@ -61,30 +61,26 @@ func move(x, n int) int {
 }
 
 func euclideanInverse(a, m int) (bool, int) {
-	n := m
-	b := a
-	q := (n - n%b) / b
-	r := n % b
-	t1 := 0
-	t2 := 1
-	t3 := t1 - q*t2
-	for r != 0 {
-		n = b
-		b = r
-		q = (n - n%b) / b
-		r = n % b
-		t1 = t2
-		t2 = t3
-		t3 = t1 - q*t2
-	}
-	if b != 1 {
-		return false, 0
-	} else {
-		for t2 < 0 {
-			t2 = t2 + m
-		}
-		return true, t2 % m
-	}
+        n := m
+        b := a
+        q := (n - n%b) / b
+        r := n % b
+        t1 := 0
+        t2 := 1
+        t3 := t1 - q*t2
+        for r != 0 {
+                n = b
+                b = r
+                q = (n - n%b) / b
+                r = n % b
+                t1 = t2
+                t2 = t3
+                t3 = t1 - q*t2
+        }
+        if b != 1 {
+                return false, 0
+        }
+        return true, move(t2, m)
 }
 
 func gcd(a, b int) int {
