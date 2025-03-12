49fc474d7a0f177bc6a371fbd70f07aa3b7bb3cf
JNGR1997 <jngrobertson@gmail.com>
2025-03-12 15:52:45 +0200

    Update modularinverse.go

--- modularinverse.go
+++ modularinverse.go
@@ -10,16 +10,16 @@ func move(x, n int) int {
 func euclideanInverse(a, n int) (int, bool) {
 	m := n
 	b := a
-	q := (m - m%b) / b
 	r := m % b
+	q := (m - r) / b
 	t1 := 0
 	t2 := 1
-	t3 := t1 - q*t2
+	t3 := -q
 	for r != 0 {
 		n = b
 		b = r
-		q = (m - m%b) / b
 		r = m % b
+		q = (m - r) / b
 		t1 = t2
 		t2 = t3
 		t3 = t1 - q*t2
