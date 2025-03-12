39946190c53b89fac38c297bc983cf64000d6910
JNGR1997 <jngrobertson@gmail.com>
2025-03-12 15:55:15 +0200

    Update ecf.go

--- ecf.go
+++ ecf.go
@@ -94,24 +94,24 @@ func move(x, n int) int {
 }
 
 func euclideanInverse(a, n int) (int, bool) {
-	m := n
-	b := a
-	q := (m - m%b) / b
-	r := m % b
-	t1 := 0
-	t2 := 1
-	t3 := t1 - q*t2
-	for r != 0 {
-		m = b
-		b = r
-		q = (m - m%b) / b
-		r = m % b
-		t1 = t2
-		t2 = t3
-		t3 = t1 - q*t2
-	}
-	if b != 1 {
-		return 0, false
-	}
-	return move(t2, n), true
+        m := n
+        b := a
+        r := m % b
+        q := (m - r) / b
+        t1 := 0
+        t2 := 1
+        t3 := -q
+        for r != 0 {
+                n = b
+                b = r
+                r = m % b
+                q = (m - r) / b
+                t1 = t2
+                t2 = t3
+                t3 = t1 - q*t2
+        }
+        if b != 1 {
+                return 0, false
+        }
+        return move(t2, n), true
 }
