3a2918f54176a81b8149a18609fad91f4f856bd2
JNGR1997 <jngrobertson@gmail.com>
2025-03-16 21:29:09 +0200

    Update twistededwardsecf.go

--- twistededwardsecf.go
+++ twistededwardsecf.go
@@ -3,63 +3,68 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(towerpoint(63, 2914, 455839, 100))
+	fmt.Println(point{x: 1, y: 1}.tower(10))
 }
 
-func towerpoint(x, y, n, m int) (int, int) {
+const n = 30758
+
+type point struct {
+	x int
+	y int
+}
+
+func (p point) tower(m int) point {
 	for m%2 == 0 {
-		x, y = doublepoint(x, y, n)
+		p = p.double()
 		m = m / 2
 	}
 
 	if m > 1 {
-		w, z := towerpoint(x, y, n, m-1)
-		x, y = addpoints(x, y, w, z, n)
+		p = p.add(p.tower(m - 1))
 	}
-	return x, y
+	return p
 }
 
-func addpoints(e, f, g, h, n int) (int, int) {
-	numx := (e*h + f*g) % n
-	numy := move(f*h-6*e*g, n)
-	denomx := (1 + 10*e*f*g*h) % n
-	denomy := move(1-10*e*f*g*h, n)
-	a, b := euclideanInverse(denomx, n)
-	c, d := euclideanInverse(denomy, n)
+func (f1 point) add(f2 point) point {
+	denomx := (1 + 10*f1.x*f1.y*f2.x*f2.y) % n
+	a, b := euclideanInverse(denomx)
 	if !a {
-		fmt.Println(denomx, " does not have an inverse mod ", n, ".")
+		return point{x: 0, y: 1}
 	}
+
+	denomy := move(1 - 10*f1.x*f1.y*f2.x*f2.y)
+	c, d := euclideanInverse(denomy)
 	if !c {
-		fmt.Println(denomy, " does not have an inverse mod ", n, ".")
+		return point{x: 0, y: 1}
 	}
-	return numx * b % n, numy * d % n
+	numx := (f1.x*f2.y + f1.y*f2.x) % n
+	numy := move(f1.y*f2.y - 6*f1.x*f2.x)
+	return point{x: numx * b % n, y: numy * d % n}
 }
 
-func doublepoint(x, y, n int) (int, int) {
-	numx := (2 * x * y) % n
-	numy := move(y*y-6*x*x, n)
-	denomx := (6*x*x + y*y) % n
-	denomy := move(2-6*x*x-y*y, n)
-	a, b := euclideanInverse(denomx, n)
-	c, d := euclideanInverse(denomy, n)
+func (f point) double() point {
+	a, b := euclideanInverse((6*f.x*f.x + f.y*f.y) % n)
 	if !a {
-		fmt.Println(denomx, " does not have an inverse mod ", n, ".")
+		return point{x: 0, y: 1}
 	}
+	c, d := euclideanInverse(move(2 - 6*f.x*f.x - f.y*f.y))
 	if !c {
-		fmt.Println(denomy, " does not have an inverse mod ", n, ".")
+		return point{x: 0, y: 1}
 	}
-	return move(numx*b, n), move(numy*d, n)
+	numx := (2 * f.x * f.y) % n
+	numy := move(f.y*f.y - 6*f.x*f.x)
+	return point{x: move(numx * b), y: move(numy * d)}
 }
 
-func move(x, n int) int {
+func move(x int) int {
 	if x < 0 {
 		return n - (-x)%n
 	}
 	return x % n
 }
 
-func euclideanInverse(a, m int) (bool, int) {
-	n := m
+func euclideanInverse(a int) (bool, int) {
+	m := n
 	b := a
 	q := (n - n%b) / b
 	r := n % b
@@ -67,16 +72,17 @@ func euclideanInverse(a, m int) (bool, int) {
 	t2 := 1
 	t3 := t1 - q*t2
 	for r != 0 {
-		n = b
+		m = b
 		b = r
-		q = (n - n%b) / b
-		r = n % b
+		q = (m - m%b) / b
+		r = m % b
 		t1 = t2
 		t2 = t3
 		t3 = t1 - q*t2
 	}
 	if b != 1 {
+		fmt.Println(a, " does not have an inverse mod ", n, ".")
 		return false, 0
 	}
-	return true, move(t2, m)
+	return true, move(t2)
 }
