b94df8e0ded3e850906b4cbd7e27636b48a96d49
JNGR1997 <jngrobertson@gmail.com>
2025-03-23 22:12:33 +0200

    Update sorting.go

--- sorting.go
+++ sorting.go
@@ -1,20 +1,19 @@
 package main
 
-import (
-	"math/rand/v2"
-)
+import "math/rand/v2"
 
 func sort(list []int, a, b int) {
 	if b-a > 0 {
 		k := a + rand.IntN(b-a)
 		list[k], list[b] = list[b], list[k]
-		z, k := a, b
-		for z < k {
-			if list[z] > list[k] {
+		z, k := b-1, b
+		for z > a-1 {
+			if list[z] < list[k] {
+				z--
+			} else {
 				list[z], list[k-1], list[k] = list[k-1], list[k], list[z]
 				k--
-			} else {
-				z++
+				z--
 			}
 		}
 		sort(list, a, k-1)
